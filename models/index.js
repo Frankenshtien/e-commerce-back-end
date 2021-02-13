3f685ddd40b502ce59b8b281d88b094468c4da47
Kyle Z Condie <barber.von.frankenshtien@gmail.com>
2021-02-12 20:27:17 -0700

    schema and seeds check. models check. associations check?

--- models/index.js
+++ models/index.js
@@ -1,17 +1,25 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
-
+Product.belongsTo(Category);
 // Categories have many Products
-
+Category.hasMany(Product);
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  as: "products_tag",
+  foreignKey: "product_id",
+});
 // Tags belongToMany Products (through ProductTag)
-
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  as: "tags_product",
+  foreignKey: "tag_id",
+});
 module.exports = {
   Product,
   Category,
